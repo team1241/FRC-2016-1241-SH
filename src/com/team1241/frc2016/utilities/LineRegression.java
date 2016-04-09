2f996a799098e9c11141771524d9dc248c142d26
Bryan Kristiono <bryan.m.kristiono@gmail.com>
2016-04-08 23:16:10 -0400

    line regression calculator working

--- src/com/team1241/frc2016/utilities/LineRegression.java
+++ src/com/team1241/frc2016/utilities/LineRegression.java
@@ -1,12 +1,16 @@
 package com.team1241.frc2016.utilities;
 
+
 public class LineRegression {
-	private double[] rpm;
-	private double[] power;
-	private static double rpmSum = 0;
-	private static double powerSum = 0;
-	private static double productSum = 0;
-	private static double squareSum = 0;
+	private int N;
+	private double[] x;
+	private double[] y;
+	private static double xSum = 0;
+	private static double ySum = 0;
+	private static double xSquaredSum = 0;
+	
+	private static double xAverage = 0;
+	private static double yAverage = 0;
 	
 	private static double slope;
 	private static double intercept;
@@ -43,24 +47,37 @@ public class LineRegression {
 	}
 	
 	public void setValues(double[] x, double[] y) {
-		this.rpm = x;
-		this.power = y;
-		rpmSum = 0;
-		powerSum = 0;
-		productSum = 0;
-		squareSum = 0;
-		for(int i=0; i<rpm.length; i++) {
-			rpmSum = rpmSum+rpm[i];
-			powerSum = powerSum+power[i];
-			productSum = productSum+ rpm[i]*power[i];
-			squareSum = squareSum + Math.pow(rpm[i], 2);
+		if (x.length != y.length) {
+            throw new IllegalArgumentException("array lengths are not equal");
+        }
+        N = x.length;
+		this.x = x;
+		this.y = y;
+		
+		xSum = 0;
+		ySum = 0;
+		xSquaredSum = 0;
+		for(int i=0; i<N; i++) {
+			xSum = xSum+x[i];
+			ySum = ySum+y[i];
+			xSquaredSum = xSquaredSum + Math.pow(x[i], 2);
 		}
+		xAverage = xSum/N;
+		yAverage = ySum/N;
+		
 		bestLine();
 	}
 	
 	private void bestLine() {
-		slope = (rpm.length*productSum-rpmSum*powerSum)/rpm.length*squareSum-Math.pow(rpmSum, 2);
-		intercept = (powerSum-slope*rpmSum)/rpm.length;
+        // second pass: compute summary statistics
+        double xxbar = 0.0, yybar = 0.0, xybar = 0.0;
+        for (int i = 0; i < N; i++) {
+            xxbar += (x[i] - xAverage) * (x[i] - xAverage);
+            yybar += (y[i] - yAverage) * (y[i] - yAverage);
+            xybar += (x[i] - xAverage) * (y[i] - yAverage);
+        }
+        slope  = xybar / xxbar;
+        intercept = yAverage - slope * xAverage;
 	}
 	
 	public double getSlope() {
@@ -73,7 +90,7 @@ public class LineRegression {
 	
 	public static void main(String[]args) {
 		LineRegression lr = new LineRegression();
-		double[] rpm = new double[]{4000, 4100, 4200, 4500, 5000};
+		int[] rpm = new int[]{4000, 4100, 4200, 4500, 5000};
 		double[] power = new double[]{0.6875, 0.705, 0.7175, 0.765, 0.8375};
 		lr.setValues(rpm, power);
 		System.out.println("Slope: "+lr.getSlope()+" intercept: "+lr.getIntercept());
